c8b018cf409339c9be4a5a59206f3f83f3b02672
Veena Logithasan <veena@ualberta.ca>
2023-03-22 05:57:56 -0600

    Created Navigation Drawer and bluetooth, log and orientation input and output fragments. Wrote the code so that the Navigation Drawer can navigate through these fragments.

--- app/src/main/java/com/example/roboticarmandroidapplication/MainActivity.kt
+++ app/src/main/java/com/example/roboticarmandroidapplication/MainActivity.kt
@@ -2,11 +2,62 @@ package com.example.roboticarmandroidapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
 
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toggle : ActionBarDrawerToggle
+    lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        val navView : NavigationView = findViewById(R.id.nav_view)
+
+        toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+
+            it.isChecked = true
+
+            when(it.itemId){
+
+                R.id.bluetooth -> replaceFragment(bluetooth(), it.title.toString())
+                R.id.orientation_in -> replaceFragment(orientation_in_out(), it.title.toString())
+                R.id.log -> replaceFragment(log(), it.title.toString())
+            }
+            true
+
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment, title : String){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout,fragment)
+        fragmentTransaction.commit()
+        drawerLayout.closeDrawers()
+        setTitle(title)
+
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (toggle.onOptionsItemSelected(item)){
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
